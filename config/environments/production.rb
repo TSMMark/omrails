666c70bba555d5d0df9aac076b8732ddd599da05
Mark Allen <shambletrain@gmail.com>
2013-04-19 23:17:30 -0400

    Add User Model with Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Omrails::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # DEVISE (SET TO ACTUAL HOST URL)
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -64,4 +67,5 @@ Omrails::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
 end
